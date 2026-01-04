58bd32fd41620993b1109c49dba79caf1d5da7c1
kamScripts <k.gebski1@gmail.com>
2026-01-04 21:53:23 +0000

    class(Person), enum(Gender), enum(Facility)

---
+++ uniHalls/src/main/java/unihalls/unihalls/Facility.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
+ */
+package unihalls.unihalls;
+
+/**
+ *
+ * @author kg00k
+ */
+public enum Facility {
+    GROUND_FLOOR_ACCESS,
+    WELLCHAIR_ACCESSIBILITY,
+    QUIET_ENVIRONMENT,
+    SUBSTANCE_FREE,
+    EMPLOYEES_FLOOR,
+    SINGLE_SEX_FLOOR,
+    HALAL,
+    VEGAN,
+    VEGETARIAN
+}
---
+++ uniHalls/src/main/java/unihalls/unihalls/Person.java
@@ -0,0 +1,91 @@
+package unihalls.unihalls;
+import java.time.LocalDateTime;
+import java.util.Collections;
+import java.util.HashSet;
+
+/**
+ *  TODO: IMPLEMENT toString()
+ * @author kg00k
+ */
+public abstract class Person {
+/**
+ * Abstract class, acting as base class for Halls Resident
+ * 
+ */
+    private String name;
+    private int age;
+    private String email;
+    private Gender gender;// Timestamp on object init.
+    private Room assignedRoom;// Assigned room in Hall.
+    private final LocalDateTime createdAt;// Account creation timestamp.
+    private final HashSet<Facility> needsList;// Set of the resident's needs.
+
+    public Person(String name, int age, String email, Gender gender) {
+        this.name = name;
+        this.age = age;
+        this.email = email;
+        this.gender = gender;        
+        createdAt = LocalDateTime.now();
+        needsList = new HashSet<>();
+    }
+    @Override
+    public String toString() {
+        return "";
+    }
+    public void addNeed(Facility [] needs) {
+        Collections.addAll(needsList, needs);
+    }
+    
+    public void removeNeed(Facility item) {
+        this.needsList.remove(item);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public Gender getGender() {
+        return gender;
+    }
+
+    public Room getAssignedRoom() {
+        return assignedRoom;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public HashSet<Facility> getNeedsList() {
+        return needsList;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setGender(Gender gender) {
+        this.gender = gender;
+    }
+
+    public void setAssignedRoom(Room assignedRoom) {
+        this.assignedRoom = assignedRoom;
+    }
+     
+}
